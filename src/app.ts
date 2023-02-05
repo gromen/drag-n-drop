03ef85f8d45e2fbfb29c375d48611662da9c97bd
Marcin Gromek <gromson101@gmail.com>
2023-02-05 09:38:07 +0100

    feat(Form): render Form in App container

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 // Code goes here!
+class ProjectInput {
+    elementTemplate: HTMLTemplateElement
+    elementHost: HTMLDivElement
+    elementForm: HTMLFormElement
+    constructor() {
+        //@ts-ignore
+        this.elementTemplate = document.getElementById('project-input')! as HTMLTemplateElement
+        this.elementHost = document.getElementById('app')! as HTMLDivElement
+        const importedNode = document.importNode(this.elementTemplate.content, true)
+        this.elementForm = importedNode.firstElementChild as HTMLFormElement
+
+        console.log(importedNode)
+        this.render()
+    }
+
+    private render() {
+        this.elementHost.insertAdjacentElement('afterbegin', this.elementForm)
+    }
+
+}
+
+const projectInput = new ProjectInput()
+
+
